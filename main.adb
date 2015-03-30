d9db5cb910523501fa2d72eac3084393de87dc2b
Obikate <f.haehnlein@gmx.net>
2015-03-30 19:17:14 +0200

    main.adb

--- main.adb
+++ main.adb
@@ -7,8 +7,64 @@ procedure main is
 	fichier: file_type;
 	h, w: float;
 	arbre_res: arbre := null;
-	type list_segments is record
 
+
+	procedure traitement_point(t: tab_point_seg; ind: integer; a: in out arbre) is
+		r: boolean := false;
+		s: segment;
+		n: arbre;
+		v_petit, v_grand: arbre;
+		c_petits, c_grands: natural;
+	begin
+		
+		-- on regarde si on est sur un point de rebroussement
+		--1) si le nombre de segments commencant au point courant est 2
+		if (t(ind).seg_1.p2.x > t(ind).p.x) and (t(ind).seg_2.p2.x > t(ind).p.x) then
+			r := true;
+			s := (t(ind).p, t(ind).p);
+			insertion(a, s, n);
+			noeuds_voisins(n, v_petit, v_grand);
+			compte_position(n, c_petits, c_grands);
+			supprimer(a, s); 
+		end if;
+
+		-- enlever les segments qui terminent sur le point courant de l'abr
+		if t(ind).seg_1.p2.x < t(ind).p.x then
+			supprimer(a, t(ind).seg_1);
+		end if;
+		if t(ind).seg_2.p2.x < t(ind).p.x then
+			supprimer(a, t(ind).seg_2);
+		end if;
+
+		-- ajouter les segments qui commencent sur le point courant dans l'abr
+		if t(ind).seg_1.p2.x > t(ind).p.x then
+			insertion(a, t(ind).seg_1, n);
+		end if;
+		if t(ind).seg_1.p2.x > t(ind).p.x then
+			insertion(a, t(ind).seg_2, n);
+		end if;
+		
+		-- si le nombre de segments terminant au point courant est 2
+		if (t(ind).seg_1.p2.x < t(ind).p.x) and (t(ind).seg_2.p2.x < t(ind).p.x) then
+			r := true;
+			s := (t(ind).p, t(ind).p);
+			insertion(a, s, n);
+			noeuds_voisins(n, v_petit, v_grand);
+			compte_position(n, c_petits, c_grands);
+			supprimer(a, s);
+		end if;
+
+		-- on traite l'éventuel point de rebroussement
+		if r then
+			if (c_petits mod 2 = 1) or (c_grands mod 2 = 1) then
+				-- reconnecter le point courant verticalement aux segments voisins
+				
+
+			end if;
+		end if;
+
+
+	end;
 
 begin
 	if argument_count /=2 then 
@@ -19,20 +75,19 @@ begin
 	lecture_nbsommets(argument(1), nb_sommets);
 
 	declare
-		tab: tab_sommets(1 .. nb_sommets);
-		poly: polygone(1 .. nb_sommets);
+		tab: tab_point_seg(1..nb_sommets);
 	begin
 
 		-- partie préparatoire
-		lecture_polygone(argument(1), tab, nb_sommets, poly);
+		lecture_tab_point_seg(argument(1), tab);
 		create(file => fichier, mode => out_file, name => argument(2));
-		svg_header(null, fichier, tab, h, w);
-		sort_point(tab);
+		svg_header(tab, h, w);
+		sort_point_seg(tab);
 		svg_polygone(fichier, tab);
 
 		--partie algo
 		for i in tab'range loop
-			ajouter_segments(tab, i, arbre_res);
+			traitement_point(tab, i, arbre_res);
 		end loop;
 
 
