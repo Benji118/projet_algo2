019e5fa51d805e3ed0753cec0c4822012c439a73
Benjamin Lebit <benjamin@benjamin-Compaq-15-Notebook-PC.(none)>
2015-03-30 17:09:10 +0200

    modifications svg pour le tracé des polygones

--- svg.adb
+++ svg.adb
@@ -1,83 +1,139 @@
-with ada.text_io, ada.integer_text_io,ada.float_text_io,objets,abr;
-use ada.text_io, ada.integer_text_io,ada.float_text_io,objets,abr;
+		with ada.text_io, ada.integer_text_io,ada.float_text_io,objets,abr;
+		use ada.text_io, ada.integer_text_io,ada.float_text_io,objets,abr;
 
-package body Svg is
+		package body Svg is
 
-function Code_Couleur (C : Color) return String is
-begin
-case C is
-when Violet => return "rgb(255,0,255)";
-when Indigo => return "rgb(111,0,255)";
-when Bleu => return "rgb(0,0,255)";
-when Vert => return "rgb(0,255,0)";
-When Jaune => return "rgb(0,255,255)";
-When Orange => return "rgb(255,165,0)";
-when Rouge => return "rgb(255,0,0)";
-when Noir => return "rgb(0,0,0)";
-when Blanc => return "rgb(255,255,255)";
-end case;
-end Code_Couleur;
+		function Code_Couleur (C : Color) return String is
+		begin
+		case C is
+		when Violet => return "rgb(255,0,255)";
+		when Indigo => return "rgb(111,0,255)";
+		when Bleu => return "rgb(0,0,255)";
+		when Vert => return "rgb(0,255,0)";
+		When Jaune => return "rgb(0,255,255)";
+		When Orange => return "rgb(255,165,0)";
+		when Rouge => return "rgb(255,0,0)";
+		when Noir => return "rgb(0,0,0)";
+		when Blanc => return "rgb(255,255,255)";
+		end case;
+		end Code_Couleur;
 
-function Width(a:arbre) return Float is
-begin
---	return largeur(a)*2; (largeur a coder)
-return 0.0;
-end;
+		
 
-function Height(a:arbre) return Float is
-begin
-	return Float(hauteur(a)*2);
+		function Height(T:Tab_Sommets) return Float is
 
-end;
+		function MaxHeight return Float is
+		Max_Height : Float:=0.0;
+		begin
+			for I in T'range loop
+				if T(I).Y>Max_Height then
+				Max_Height:=T(I).Y;
+				end if;
+			end loop;
+			return Max_Height;
+		end MaxHeight;
+
+			function MinHeight return Float is
+		Min_Height : Float := 0.0;
+		begin
+			for I in T'range loop
+			if T(I).Y<Min_Height then
+			Min_Height := T(I).Y;
+			end if;
+		end loop;
+		return Min_Height;
+		end MinHeight;
+
+		begin
+			return MaxHeight - MinHeight;
+		end Height;
+
+		function Width(T:Tab_Sommets) return Float is
+
+			function MaxWidth return Float is
+		Max_Width : Float:=0.0;
+		begin
+		for I in T'range loop
+			if T(I).X>Max_Width then
+			Max_Width := T(I).X;
+			end if;
+		end loop;
+		return Max_Width;
+		end MaxWidth;
+
+		function MinWidth return Float is
+		Min_Width : Float := 0.0;
+		begin
+			for I in T'range loop
+			if T(I).X<Min_Width then
+			Min_Width := T(I).X;
+			end if;
+		end loop;
+		return Min_Width;
+		end MinWidth;
+
+		begin
+			return MaxWidth - MinWidth;
+		end Width;
 
 
- procedure Svg_Header(a: arbre ; Fichier_Svg : in out File_Type ; H : out Float; W : out Float) is
-begin
-W:=Width(a);
-H:=100.0; --(a remplacer par Height(a))
+		 procedure Svg_Header(W,H : in Float) is
+		begin
+		Put (Fichier_Svg, "<svg width=""");
+		Put (Fichier_Svg, W);
+		Put (Fichier_Svg, """ height=""");
+		Put (Fichier_Svg, H);
+		Put_Line (Fichier_Svg, """>");
+		end Svg_Header;
 
-Put (Fichier_Svg, "<svg width=""");
-Put (Fichier_Svg, W);
-Put (Fichier_Svg, """ height=""");
-Put (Fichier_Svg, H);
-Put_Line (Fichier_Svg, """>");
-end Svg_Header;
+		procedure Svg_Footer is
+		begin
+		Put_Line (Fichier_Svg, "</svg>");
+		end Svg_Footer;
 
-procedure Svg_Footer(Fichier_Svg : in out File_Type) is
-begin
-Put_Line (Fichier_Svg, "</svg>");
-end Svg_Footer;
+		procedure Svg_Line (A, B : Point; C: Color;Fichier_Svg : in out File_Type)
+		is
+		begin
+		Put (Fichier_Svg, "<line x1=""");
+		Put (Fichier_Svg, A.X);
+		Put (Fichier_Svg, """ y1=""");
+		Put (Fichier_Svg, A.Y);
+		Put (Fichier_Svg, """ x2=""");
+		Put (Fichier_Svg, B.X);
+		Put (Fichier_Svg, """ y2=""");
+		Put (Fichier_Svg, B.Y);
+		Put (Fichier_Svg, """ style=""stroke:");
+		Put (Fichier_Svg, Code_Couleur(C));
+		Put_Line (Fichier_Svg, ";stroke-width:0.1""/>");
+		end Svg_Line;
 
-procedure Svg_Line (A, B : Point; C: Color;Fichier_Svg : in out File_Type)
-is
-begin
-Put (Fichier_Svg, "<line x1=""");
-Put (Fichier_Svg, A.X);
-Put (Fichier_Svg, """ y1=""");
-Put (Fichier_Svg, A.Y);
-Put (Fichier_Svg, """ x2=""");
-Put (Fichier_Svg, B.X);
-Put (Fichier_Svg, """ y2=""");
-Put (Fichier_Svg, B.Y);
-Put (Fichier_Svg, """ style=""stroke:");
-Put (Fichier_Svg, Code_Couleur(C));
-Put_Line (Fichier_Svg, ";stroke-width:0.1""/>");
-end Svg_Line;
+		procedure Svg_Grid(T: in Tab_Sommets) is
+		W,H:Float;
+		begin
 
-procedure Coord_Noeud(N:noeud;P:out Point) is
-begin
-	if N.pere=null then
-		P:=(50.0,50.0);
-	elsif N.c >N.pere.c then
-		P:=(Coord_Noeud(N.pere.all,P).X+5.0,Coord_Noeud(N.pere.all,P).Y+10.0);
-	elsif N.c <= N.pere.c then
-		P:=(Coord_Noeud(N.pere.all,P).X+5.0,Coord_Noeud(N.pere.all,P).Y+10.0);
-	end if;
-end Coord_Noeud;
+		W:=Width(T);
+		H:=Height(T);
 
-procedure Dessiner_abr(a: in arbre;Tab_noeuds : Tab_Sommets) is
-begin
-	null;
-end;
+		Put (Fichier_Svg,"<rect x=""");
+		Put (Fichier_Svg,50.0);
+		Put (Fichier_Svg,""" y=""");
+		Put (Fichier_Svg,50.0);
+		Put (Fichier_Svg,""" width=""");
+		Put (Fichier_Svg,W);
+		Put (Fichier_Svg,""" height=""");
+		Put (Fichier_Svg,H);
+		Put(Fichier_Svg,""" style=""");
+		Put(Fichier_Svg,"fill:white;stroke:black;stroke-width:2px;");
+		Put_Line(Fichier_Svg,"""/>""");
+		end Svg_Grid;
 
-end Svg;
+		procedure svg_polygone(Fichier_Svg: in File_Type; tab: Tab_Sommets) is begin
+		 Put (Fichier_Svg, "<polygon points=""");
+			  for i in tab'range loop
+			  Put (Fichier_Svg, tab(i).X);
+			  Put (Fichier_Svg, ",");
+			  Put (Fichier_Svg, tab(i).Y); Put (Fichier_Svg, " ");
+		      end loop;
+		 Put_Line(Fichier_Svg, """ style=""fill:lime;stroke:purple;stroke-width:0.1"" /> "); end;
+
+		end Svg;
--- svg.ads
+++ svg.ads
@@ -7,20 +7,20 @@ Fichier_Svg : File_Type;
 
 type Color is (Violet, Indigo, Bleu, Vert, Jaune, Orange, Rouge, Noir, Blanc);
 
-function Width(a:arbre) return Float;
+function Width(T:Tab_Sommets) return Float;
 
-function Height(a:arbre) return Float;
-
-procedure Coord_Noeud(N:noeud;P:out Point);
-
-procedure Dessiner_abr(a: in arbre;Tab_noeuds : Tab_Sommets);
+function Height(T:Tab_Sommets) return Float;
 
 function Code_Couleur (C : Color) return String;
 
-procedure Svg_Header(a:arbre;Fichier_Svg : in out File_Type;H : out Float;W : out Float);
+procedure Svg_Header(W,H : in FLoat);
 
-procedure Svg_Footer(Fichier_Svg : in out File_Type);
+procedure Svg_Footer;
 
 procedure Svg_Line (A, B : Point; C: Color;Fichier_Svg : in out File_Type);
+
+procedure Svg_Grid (T: in Tab_Sommets);
+
+procedure svg_polygone(Fichier_Svg: in File_Type; tab: Tab_Sommets);
 
 end Svg;
