a3f9e0ba07301ae05595893f3b2631f9775830a5
Obikate <f.haehnlein@gmx.net>
2015-04-04 14:37:22 +0200

    commentaires1

--- parseur.adb
+++ parseur.adb
@@ -3,14 +3,16 @@ use ada.text_io, ada.integer_text_io, objets,ada.float_text_io;
 
 package body parseur is
 
-	fichier_in : file_type;
-
 	procedure lecture_nb_sommets(nom_fichier: in string; nb_sommets: out Integer) is
+
+		fichier_in: file_type;
+
 	begin
 		open ( file => fichier_in,
 		mode => in_file,
 		name => nom_fichier);
 
+		-- on lit le nombre de sommets qui est le premier nombre du fichier
 		get(fichier_in, nb_sommets);
 
 		close(fichier_in);
@@ -18,38 +20,47 @@ package body parseur is
 	end lecture_nb_sommets;
 
 	procedure lecture_tab_point_seg(nom_fichier: in string; t: out tab_point_seg) is
-	
+
+		fichier_in: file_type;
+
 	begin
 
 		open ( file => fichier_in,
 		mode => in_file,
 		name => nom_fichier);
 
+		-- on a déjà lu la première ligne dans la procédure lecture_nb_sommets
 		skip_Line(fichier_in);
 
 
-			for i in t'range loop
-				get(fichier_in, t(i).p.x);
-				get(fichier_in, t(i).p.y);
-			end loop;
-
-			for i in 1..t'Last-1 loop
-				t(i).seg1.p1:=t(i).p;
-				t(i).seg1.p2:=t(i + 1).p;
-			end loop;
-
-			t(t'last).seg1.p1:=t(t'last).p;
-			t(t'last).seg1.p2:=t(t'first).p;
-
-			for i in t'first + 1..t'Last loop
-				t(i).seg2.p1:=t(i).p;
-				t(i).seg2.p2:=t(i - 1).p;
-			end loop;
-
-			t(t'first).seg2.p1:=t(t'first).p;	
-			t(t'first).seg2.p2:=t(t'last).p;
-
+		-- le parcours du fichier sert à sauvegarder tous les points dans le tab_point_seg
+		for i in t'range loop
+			get(fichier_in, t(i).p.x);
+			get(fichier_in, t(i).p.y);
+		end loop;
 		close(fichier_in);
+
+		-- les points voisins d'un point dans le polygon sont définis par l'ordre 
+		-- des points dans le fichier d'entrée. Or, on va trier le tableau dans le programme 
+		-- principal. Ainsi, il est important de se rappeler du voisinage de chaque point
+		for i in 1..t'Last-1 loop
+			t(i).seg1.p1:=t(i).p;
+			t(i).seg1.p2:=t(i + 1).p;
+		end loop;
+		-- le dernier point doit être traîte à part pour ne pas violer la "range" du tableau
+		-- rmq: on aura pu se débrouiller avec un mod, mais on a préféré cette méthode car elle
+		-- nous semblerait être plus lisible
+		t(t'last).seg1.p1:=t(t'last).p;
+		t(t'last).seg1.p2:=t(t'first).p;
+
+		-- pareil pour les voisins dans l'autre sens
+		for i in t'first + 1..t'Last loop
+			t(i).seg2.p1:=t(i).p;
+			t(i).seg2.p2:=t(i - 1).p;
+		end loop;
+		t(t'first).seg2.p1:=t(t'first).p;	
+		t(t'first).seg2.p2:=t(t'last).p;
+
 
 	end lecture_tab_point_seg;
 
--- parseur.ads
+++ parseur.ads
@@ -1,10 +1,20 @@
 with objets;
 use objets;
 
+-- le paquetage lit le fichier d'entrée et sauvegarde les informations dans un tab_point_seg (défini 
+-- dans objets.ads)
 package parseur is
 
+	-- procédure qui lit uniquement la première ligne du fichier d'entrée
+	-- on en a besoin dans le programme principal pour pouvoir initialiser le tab_point_seg
 	procedure lecture_nb_sommets(nom_Fichier: in string; nb_sommets: out integer);
+	-- req: un fichier qui suit le benchmark .in
+	-- gar: le fichier reste inchangé, le premier nombre de la première ligne sera stocké dans nb_sommets
 
+	-- procédure qui parse le fichier entier à partir de la deuxième ligne
 	procedure lecture_tab_point_seg(nom_Fichier: in String; t: out tab_point_seg);
+	-- req: un fichier qui suit le benchmar .in
+	-- gar: le fichier reste inchangé, le tableau tab_point_seg contiendra tous les points du polygone 
+	-- et chaque point disposera des informations sur ses voisins dans le polygone
 
 end parseur;
